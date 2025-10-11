27934da517efcd6eadd6a2e997c69a84773f8844
Sean <sean894025669@yahooe.com>
2025-10-11 05:06:50 -0400

    chore: initial Spring Boot setup and HelloController

---
+++ src/test/java/com/sean/restaurant/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
